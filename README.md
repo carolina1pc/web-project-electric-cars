df52be9ae7f7bb1c7334a8ba136d4783dfe78162
t3ni3 <karolinapetrovici9@gmail.com>
2025-10-31 15:55:20 +0200

    Upload project

---
+++ README.md
@@ -0,0 +1,1 @@
+# web-project-electric-cars
---
+++ script.js
@@ -0,0 +1,21 @@
+function openTab(evt, tabName) {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var defaultTab = document.getElementsByClassName("tablinks")[0];
+    if (defaultTab) {
+        defaultTab.click();
+    }
+});
